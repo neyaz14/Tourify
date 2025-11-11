6804ffd2927a76819c1a0bc48c146e29ce0cce6a
Neyaz Morshid <neyazmorshid@gmail.com>
2025-11-12 02:36:48 +0600

    added sslCommerze initial file

--- backend/src/app/modules/payment/payment.routes.ts
+++ backend/src/app/modules/payment/payment.routes.ts
@@ -1,3 +1,9 @@
 import { Router } from "express";
+import { paymentController } from "./payment.controller";
 
 export const paymentRouter = Router()
+
+
+paymentRouter.get('/initialPayment'
+    , paymentController.initPayment
+)
---
+++ backend/src/app/modules/payment/payment.service.ts
@@ -0,0 +1,29 @@
+
+
+const initPayment =async (bookingId: string)=>{
+
+    return {}
+}
+
+
+const successPayment = async (query: Record<string,string>)=>{
+
+    return {}
+}
+
+const cancelPayment = async (query: Record<string,string>)=>{
+
+    return {}
+}
+
+const failPayment = async (query: Record<string,string>)=>{
+
+    return {}
+}
+
+
+
+export const paymentService = {
+
+    initPayment , successPayment , cancelPayment, failPayment
+}
